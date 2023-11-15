e49e2098b0eca3e59e96366c14a7c5dec488f326
Mslonoff <matthew.slonoff@gmail.com>
2023-11-15 18:21:53 -0500

    flashing done

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-
 let container = document.createElement('div');
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 container.style.height = '800px';
 container.style.width = '800px';
 
-
-function createRedSquare() {
+function createFlashingSquare() {
 let square = document.createElement('div');
 square.style.display = 'flex';
 square.style.flexWrap = 'wrap';
 square.style.height = '12.5%';
 square.style.width = '12.5%';
-square.style.background = 'red';
 container.appendChild(square);
 document.body.appendChild(container);
+
+function setRandomColor() {
+    let randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+    square.style.background = randomColor;
+}
+setRandomColor;
+setInterval(setRandomColor, 2000);
 }
 
-function createBlackSquare() {
-let square = document.createElement('div');
-square.style.display = 'flex';
-square.style.flexWrap = 'wrap';
-square.style.height = '12.5%';
-square.style.width = '12.5%';
-square.style.background = 'black';
-container.appendChild(square);
-document.body.appendChild(container);
-}
 for (let i = 0; i < 64; i++) {
-if ((Math.floor(i / 8) + i) % 2 === 0) {
-    createRedSquare();
-} else {
-    createBlackSquare();
+  createFlashingSquare();
 }
-}
-
-
-
-// Create a container for your tiles with the following styles:
-// display = "flex"
-// flexWrap = "wrap"
-// width = "800px"
-// height = "800px"
-// Each tile should have the following styles:
-// width = "12.5%"
-// height = "12.5%"
